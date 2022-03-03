84927e750e06796b011f0567b8b5956f58f43e53
Ted Bonga <tedbonga4.tb@gmail.com>
2022-03-03 09:05:49 +0100

    Add player and new script
    Player script which i can move the player

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        // Reset ModeDelta
+        moveDelta = new Vector3(x,y,0);
+
+        //Swap sprite direction,wether you're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Make this thing move !
+        transform.Translate(moveDelta * Time.deltaTime);
+
+    }
+}
